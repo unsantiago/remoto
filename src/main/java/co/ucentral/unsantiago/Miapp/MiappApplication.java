5ed438a3ff0020010b171e7a12ffdfe562e8e0f3
unsantiago <lprietoa2@gmail.com>
2024-03-21 20:51:35 -0500

    Este es el primer commit

---
+++ src/main/java/co/ucentral/unsantiago/Miapp/MiappApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.unsantiago.Miapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/ucentral/unsantiago/Miapp/MiappApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ucentral.unsantiago.Miapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
